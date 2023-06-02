cab02021a35d29e197808a41cdef2c34f890d98b
Kadir Nar <kadirnar@users.noreply.huggingface.co>
2023-06-02 20:44:18 +0000

    Upload 15 files

---
+++ diffusion_webui/utils/model_list.py
@@ -0,0 +1,36 @@
+stable_model_list = [
+    "runwayml/stable-diffusion-v1-5",
+    "stabilityai/stable-diffusion-2-1",
+    "prompthero/openjourney-v4",
+    "dreamlike-art/dreamlike-diffusion-1.0",
+]
+
+stable_inpiant_model_list = [
+    "stabilityai/stable-diffusion-2-inpainting",
+    "runwayml/stable-diffusion-inpainting",
+    "saik0s/realistic_vision_inpainting",
+]
+
+controlnet_model_list = [
+    #"lllyasviel/sd-controlnet-canny",
+    "lllyasviel/control_v11p_sd15_canny",
+    "thibaud/controlnet-sd21-canny-diffusers",
+    #"lllyasviel/sd-controlnet-depth",
+    "lllyasviel/control_v11f1p_sd15_depth",
+    "thibaud/controlnet-sd21-depth-diffusers",
+    #"lllyasviel/sd-controlnet-openpose",
+    "lllyasviel/control_v11p_sd15_openpose",
+    "thibaud/controlnet-sd21-openpose-diffusers",
+    #"lllyasviel/sd-controlnet-hed",
+    "thibaud/controlnet-sd21-hed-diffusers",
+    #"lllyasviel/sd-controlnet-scribble",
+    "lllyasviel/control_v11p_sd15_scribble",
+    "thibaud/controlnet-sd21-scribble-diffusers",
+    #"lllyasviel/sd-controlnet-mlsd",
+    "lllyasviel/control_v11p_sd15_mlsd",
+    "lllyasviel/control_v11e_sd15_shuffle",
+    "lllyasviel/control_v11e_sd15_ip2p",
+    "lllyasviel/control_v11p_sd15_lineart",
+    "lllyasviel/control_v11p_sd15s2_lineart_anime",
+    "lllyasviel/control_v11p_sd15_softedge",
+]
