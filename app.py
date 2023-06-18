49ae800ffdf8e9112522e1f2a9bcca5de4f91b50
Kadir Nar <kadirnar@users.noreply.huggingface.co>
2023-06-18 17:52:35 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,21 @@ from diffusion_webui import (
 def diffusion_app():
     app = gr.Blocks()
     with app:
+        gr.HTML(
+        """
+        <h1 style='text-align: center'>
+       Stable Diffusion Infinite Zoom Out and Zoom In
+        </h1>
+        """
+    )
+        gr.HTML(
+            """
+            <h3 style='text-align: center'>
+            Follow me for more! 
+            <a href='https://twitter.com/kadirnar_ai' target='_blank'>Twitter</a> | <a href='https://github.com/kadirnar' target='_blank'>Github</a> | <a href='https://www.linkedin.com/in/kadir-nar/' target='_blank'>Linkedin</a>
+            </h3>
+            """
+    )
         with gr.Row():
             with gr.Column():
                 with gr.Tab(label="Text2Image"):
